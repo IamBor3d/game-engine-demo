8ae5f1a65a86a66c16c89ace372c2bc6aa17db54
Aaman Ali <aaman.ali31106@gmail.com>
2026-04-14 16:55:02 +0100

    Add whole window canvas rendering.

--- components/gameScreen.tsx
+++ components/gameScreen.tsx
@@ -1,24 +1,30 @@
 "use client"
 import { GameDemo } from "@/game_demo/gameDemo";
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export default function Game(){
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
-    const canvasWidth : number = 1000;
-    const canvasHeight : number = 1000;
+    const [dimensions, setDimensions] = useState({width : 0, height : 0});
+    
+    useEffect(() => {
+        setDimensions({
+            width: window.innerWidth,
+            height: window.innerHeight
+        })},[]);
 
     useEffect(()=>{
+        
         const canvas = canvasRef.current;
-        if (!canvas) return;
+        if (!canvas || dimensions.width === 0) return;
 
-        const game = new GameDemo(canvasHeight,canvasWidth, canvas);
+        const game = new GameDemo(dimensions.height,dimensions.width, canvas);
         game.start();
         return ()=>game.destroy();
-    },[]);
+    },[dimensions]);
     return(
         <div>
             <div id="screen">
-                <canvas ref={canvasRef} width={canvasWidth} height={canvasHeight}/>
+                <canvas ref={canvasRef} width={dimensions.width} height={dimensions.height}/>
             </div>
         </div>
     )
